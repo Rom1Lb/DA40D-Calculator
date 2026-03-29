408f6d36ee72696bd23bd66cb48bde05eac6d18d
Romain <114222495+Rom1Lb@users.noreply.github.com>
2026-03-29 13:05:45 +0200

    style: add responsive breakpoints and bg3 token

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { createRoot }     from "react-dom/client";
-import { BrowserRouter }  from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/global.css";
-import App                from "./App.jsx";
-import { ErrorBoundary }  from "./ErrorBoundary.jsx";
+import "./styles/additions.css"; // ← ajouter cette ligne
+import App from "./App.jsx";
+import { ErrorBoundary } from "./ErrorBoundary.jsx";
 
 createRoot(document.getElementById("root")).render(
   <ErrorBoundary>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </ErrorBoundary>
+  </ErrorBoundary>,
 );
